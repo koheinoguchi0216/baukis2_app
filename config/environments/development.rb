630fa22a26a879f4fb30cd40d9b60f4819f87b84
koheinoguchi0216 <i.kohei.noguchi@globis.com>
2022-02-21 10:53:05 +0900

    baukis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.web_console.whitelisted_ips = ['172.16.0.0/12']
+  config.web_console.whitelisted_ips = ['192.169.60.0/12']
 end
